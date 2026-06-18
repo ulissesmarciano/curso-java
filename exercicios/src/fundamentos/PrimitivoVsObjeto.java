02aa82156c07690e3c8633ee8eb6b6f4f175f41b
Ulisses Marciano <ulissesmarcianodev@gmail.com>
2026-06-18 16:05:08 -0300

    add PrimitivoVsObjetivo class

--- exercicios/src/fundamentos/PrimitivoVsObjeto.java
+++ exercicios/src/fundamentos/PrimitivoVsObjeto.java
@@ -4,5 +4,11 @@ public class PrimitivoVsObjeto {
     public static void main(String[] args) {
         String s = new String("texto");
         s.toUpperCase();
+
+        // Wrappers são a versão objeto dos tipos
+        // primitivos!
+        int a =  123;
+        System.out.println(a);
+
     }
 }
