18f6850f3a4b39aec73b174a1f9681eebe372b8d
Ulisses Marciano <ulissesmarcianodev@gmail.com>
2026-06-23 17:50:04 -0300

    add DesafioIf class

---
+++ exercicios/src/controle/DesafioIf.java
@@ -0,0 +1,15 @@
+package controle;
+
+public class DesafioIf {
+    public static void main(String[] args) {
+
+        double nota = 1.3;
+
+
+        if(nota >= 9.0); // o erro deste desafio está no ponto e vírgula que finaliza a sentença posta e o bloco é executado de forma descolada do if.
+        {
+            System.out.println("Quadro de Honra!");
+            System.out.println("Você é fera!");
+        }
+    }
+}
