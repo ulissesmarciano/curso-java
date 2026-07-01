d55b9a9923c86e80abf176ae1a5bfc9ed723e8dd
Ulisses Marciano <ulissesmarcianodev@gmail.com>
2026-07-01 15:09:08 -0300

    add Produto ProdutoTeste exercise

--- exercicios/src/classe/ProdutoTeste.java
+++ exercicios/src/classe/ProdutoTeste.java
@@ -3,15 +3,15 @@ package classe;
 public class ProdutoTeste {
     public static void main(String[] args) {
 
-        Produto p1 = new Produto("Notebook", 4356.54, 0.29);
+        Produto p1 = new Produto("Notebook", 4356.54);
 
         var p2 = new Produto();
         p2.nome = "Caneca Preta";
         p2.preco = 12.56;
-        p2.desconto = 0.29;
+        Produto.desconto = 0.29;
 
-        System.out.println(p1.nome);
-        System.out.println(p2.nome);
+        System.out.println(p1.nome + " " + p1.precoComDesconto());
+        System.out.println(p2.nome + " " + p2.precoComDesconto());
 
         double precoFinal1 = p1.precoComDesconto();
         double precoFinal2 = p2.precoComDesconto(0.1);
